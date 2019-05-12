f386248187143485273e9cd6a8670df84fa751dd
lucasbrito92 <lucas.brito92@live.com>
2019-05-12 16:57:27 -0300

    final code to true table

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import itertools
 from prettytable import PrettyTable
 
+
 def search(A):
     if A in gates:
         return gates[A]
@@ -17,65 +18,6 @@ def operate(A, op, B):
         return A ^ B
 
 
-def print_file(truth):
-
-    with open(filename+'_result.txt', 'w') as f:
-        # printa rotulo das variaveis
-        for line_head in header:
-            for v in line_head:
-                f.write(v)
-                f.write(" ")
-
-        # printar valores da tabela
-        for line_truth in truth:
-            for v in line_truth:
-                if type(v) == bool:
-                    f.write("{}".format(int(v)))
-                    f.write(" ")
-                else:
-                    f.write(v)
-                    f.write(" ")
-                    f.write("\n")
-            f.write("\n")
-
-    f.close()
-
-
-def translate(type):
-
-    if type == "presaEm1":
-        return True
-    else:
-        return False
-
-
-""" def b_search(A):
-
-    if A in blacklist:
-        return blacklist[A]
-    else:
-        return search(A) """
-
-
-""" def insert_failures_in(perm_entries, table):
-
-    # processando falhas [...]
-    new = {}
-    for op in perm_entries:
-        gates.update({line_gate[2]: operate(
-            b_search(line_gate[-2]), line_gate[1], b_search(line_gate[-1]))})
-
-    for line in perm_entries:
-        for line_gate in gates_aux:
-            gates.update({line_gate[2]: operate(
-                b_search(line_gate[-2]), line_gate[1], b_search(line_gate[-1]))})
-
-    values = merge_line(ent, gates, "v")
-    truth.append(values)
-
-    return truth """
-
-
 def generate_truth_table(perm_entries):
 
     for line in perm_entries:
@@ -90,7 +32,7 @@ def generate_truth_table(perm_entries):
             gates.update({line_gate[2]: operate(
                 search(line_gate[-2]), line_gate[1], search(line_gate[-1]))})
 
-        # merge dicts *only python > 3.5* 
+        # merge dicts *only python > 3.5*
         line_table = {**ent, **gates}
         truth.append(list(line_table.values()))
         # print_file(truth)
@@ -128,11 +70,11 @@ if __name__ == "__main__":
 
     f.close()
 
-    # gera permutações das entradas e aplica nas func logicas da tabela
+    # generates permutations of the inputs and applies in the functions of the table
     perm_entries = list(itertools.product([False, True], repeat=int(n_ent[1])))
     table = generate_truth_table(perm_entries)
-    
-    # cria tabela e escreve no arquivo
+
+    # create table and write to file
     tt = PrettyTable()
     tt.field_names = header
 
@@ -140,48 +82,9 @@ if __name__ == "__main__":
         tam = int(len(header))
         index = line[:tam]
         tt.add_row(index)
-    
+
     table_txt = tt.get_string()
-    with open(filename+'_output.txt','w') as file:
+    with open(filename+'_output.txt', 'w') as file:
         file.write(table_txt)
 
-    # inserir dic de entradas e saidas imutaveis ou "presas"
-    # nesse caso, fazer permutações do n de entradas e saidas e falhas
-
-    # gerar combinações de entradas com um valor fixo
-    perm_entries_fails = list(itertools.product([False, True], repeat=int(n_ent[1])))
-
-    v_aux = tuple(True)
-    for line in perm_entries_fails:
-        values = list(line)
-        _v =  list(v_aux) + values[1:]
-
-    """ filename_fail = filename+"_fail"
-    f = open(filename+"_fail.txt", "r")
-    n_fails = f.readline().split(",")
-    truth, fails_aux = [], []
-
-    for x in f:
-        fails = x.split(",")
-        fails[-1] = fails[-1].split("\n")[0]
-        fails_aux.append(fails)
-
-    f.close() """
-
-    # fazer uma matriz de de portas por falhas
-
-    # melhor construir um dict blacklist e consutar a partir dele
-
-    # blacklist = {}
-
-    # possible_failures_list=list(itertools.product([False, True], repeat = int(n_fails)))
-
-    # inserindo falhas especificadas [...]
-    # for fail_line in fails_aux:
-    #    blacklist.update({str(fail_line[0]): translate(fail_line[1])})
-
-    # função para gerar tabela com falhas de acordo com o arquivo de especificações
-    # failure_table = insert_failures_in(perm_entries, table)
-
-    # fazer função para diagnosticar falhas
     print("well done")
